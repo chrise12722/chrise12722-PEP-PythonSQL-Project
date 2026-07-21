cc2bcfc336c75a008a5be65457729281f83dd14d
chrise12722 <chrise12722@github.com>
2026-07-21 19:35:54 +0000

    Test run: 2/4 passed, 2 failed, 0 skipped - 2026-07-21T19:35:54.164Z

--- src/main/main.py
+++ src/main/main.py
@@ -100,19 +100,19 @@ def load_and_clean_call_logs(file_path):
 def write_user_analytics(csv_file_path):
     cursor.execute("SELECT COUNT(*) FROM users")
     user_count = cursor.fetchone()[0]
-    cursor.execute("SELECT * FROM callLogs")
+    cursor.execute("SELECT userId, startTime, endTime FROM callLogs")
     call_logs = cursor.fetchall()
     log_counter = 0
     user_data = {}
     # Store each row in user_data dictionary, where key = userId and value = list [list of call durations to avg later, num of calls]
     for row in call_logs:
-        user_id = call_logs[log_counter][5]
+        user_id = call_logs[log_counter][0]
         log_counter += 1
         if user_id in user_data.keys():
-            user_data[user_id][0].append(float(row[3]) - row[2])
+            user_data[user_id][0].append(float(row[2]) - row[1])
             user_data[user_id][1] += 1
         else:
-            user_data[user_id] = [[float(row[3]) - row[2]], 1]
+            user_data[user_id] = [[float(row[2]) - row[1]], 1]
     # Find average for each user's call durations
     for user_id, data in user_data.items():
         durations = data[0]
